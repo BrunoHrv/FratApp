7288410e52daee568a1688b5cbe7c908aea20bb1
NJacob <kn2ply@gmail.com>
2016-03-29 14:51:23 -0400

    user directory and user pages

--- FratApp/directory/views.py
+++ FratApp/directory/views.py
@@ -10,13 +10,17 @@ from django.contrib.auth import logout
 def index(request):
 	if request.user.is_authenticated():
 		user = request.user
-		print user
 		context={
 			'username':user.username,
 			'firstname':user.first_name,
 			'lastname':user.last_name,
 		}
-		print context
+		if request.method == 'GET':
+			if 'user' in request.GET:
+				userquery=User.objects.get(username=request.GET['user'])
+				context['userquery']=userquery
+				return render(request, 'Directory/user.html', context)
+		context['userlist']=User.objects.all()
 		if request.method== 'POST' and 'logout' in request.POST:
 			logout(request)
 			return redirect('/')
