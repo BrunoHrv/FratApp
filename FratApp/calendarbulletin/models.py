766805ae878156a980903d515f690cb6950a500a
emperorpik <smithn8@rpi.edu>
2016-04-01 19:44:43 -0400

    Updated calendatbulletin's models.py
    created announcement class

--- FratApp/calendarbulletin/models.py
+++ FratApp/calendarbulletin/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+#Class for making announcements
+class Announcement(models.model):
+  creator = models.CharField(Max_Length = 200)
+  title = models.CharField(Max_Length = 200)
+  text = models.TextField(Max_Length = 200)
+  postDate = models.DateTimeField (default = datetime.now, editable = False)
+  
+  #Orders all queries with the most recently created first
+  class Meta:
+    ordering = ('-datetime')
+  
+  #Recommended Unicode method
+  def __unicode__(self):
+       return unicode(self.title)
