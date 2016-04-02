a94eba47dffcb6d7c1a303cee4ec00750ebdb084
Nevin Jacob <kn2ply@gmail.com>
2016-04-01 21:31:34 -0400

    calendar

--- FratApp/calendarbulletin/views.py
+++ FratApp/calendarbulletin/views.py
@@ -4,12 +4,27 @@ from django.template import loader
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
+import httplib2
+import os
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.service_account import ServiceAccountCredentials
+from httplib2 import Http
+from apiclient.discovery import build
 
-# Create your views here.
+#scopes = ['https://www.googleapis.com/auth/sqlservice.admin', 'https://www.googleapis.com/auth/calendar']
+#credentials = ServiceAccountCredentials.from_json_keyfile_name('FratApp-3a6a3a856702.json', scopes=scopes)
+#http_auth = credentials.authorize(Http())
+#caladmin = build('calendar', 'v3', http=http_auth)
+#APPLICATION_NAME = 'FratApp'
 
 def index(request):
 	if request.user.is_authenticated():
 		user = request.user
+		#delegated_credentials = credentials.create_delegated(user.email)
+		#user_auth = delegated_credentials.authorize(Http())
 		context={
 			'username':user.username,
 			'firstname':user.first_name,
