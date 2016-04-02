9d0a0ac630ac64e3efd99a08dff0a5bfb663cff0
emperorpik <smithn8@rpi.edu>
2016-04-01 20:34:42 -0400

    created Attendee class

--- FratApp/attendancelists/models.py
+++ FratApp/attendancelists/models.py
@@ -3,3 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+#Class for Attendees for attendance lists
+class Attendee (models.Model):
+  firstName = models.CharField(max_length = 25)
+  lastName = models.CharField(max_length = 25)
+  affiliation = models.CharField(max_length = 25)
+  
+  #Orders all queries by last name
+  class Meta:
+    ordering = ('lastName')
+    
+  def __unicode__(self):
+    return unicode(self.lastName)
