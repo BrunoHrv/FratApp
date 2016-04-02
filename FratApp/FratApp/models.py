ab89507c025f4933613ac8727249259f84106a2c
emperorpik <smithn8@rpi.edu>
2016-04-01 21:28:24 -0400

    started FratAppUser

--- FratApp/FratApp/models.py
+++ FratApp/FratApp/models.py
@@ -13,6 +13,18 @@ class FlowModel(models.Model):
 class CredentialsModel(models.Model):
   id = models.ForeignKey(User, primary_key=True)
   credential = CredentialsField()
+  
+class FratAppUser(AbstractBaseUser):
+  
+  email - models.EmailField(_('email address'), max_length = 254, unique = True)
+  firstName = models.CharField(_('first name'), max_length = 25)
+  lastName = models.CharField(_('last name'), max_length = 25)
+  title = models.CharField(_('title'), max_length = 25, blank = True)
+  hometown = models.Charfield(_('title'), max_length = 50, blank = True)
+  major = models.Charfield(_('major'), max_length = 25, blank = True)
+  
+  
+  
 
 '''
 user = # A User object usually obtained from request.
