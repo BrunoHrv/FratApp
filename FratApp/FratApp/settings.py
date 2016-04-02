47bdb79a3dcd6394202a1d88eff4e7c178b65fea
emperorpik <smithn8@rpi.edu>
2016-04-02 19:49:50 -0400

    added installed apps

--- FratApp/FratApp/settings.py
+++ FratApp/FratApp/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calendarbulletin',
+    'FratApp',
+    'directory',
+    'landing',
+    'tasks',
+    
 ]
 
 MIDDLEWARE_CLASSES = [
