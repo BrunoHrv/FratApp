332a9187c551d3ef9634522bf3922a060062b0e8
= <=>
2016-04-06 16:20:36 -0400

    added support for event creation and attendee creation

--- FratApp/attendancelists/models.py
+++ FratApp/attendancelists/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime  
 
 # Create your models here.
+class Event(models.Model):
+	id=models.AutoField(primary_key=True)
+	creator = models.CharField(max_length = 200)
+  	title = models.CharField(max_length = 200)
+  	text = models.TextField(max_length = 200)
+  	postDate = models.DateField (auto_now_add=True, editable = False)
+
+  	#Orders all queries with the most recently created first
+  	class Meta:
+  	 	ordering = ['-postDate']
+  	
+  	#Recommended Unicode method
+  	def __unicode__(self):
+  	    return unicode(self.title)
+
+class Attendee(models.Model):
+	event = models.ForeignKey(Event, on_delete=models.CASCADE)
+	name = models.CharField(max_length = 200)
--- FratApp/attendancelists/views.py
+++ FratApp/attendancelists/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
+from models import *
 
 # Create your views here.
 
@@ -14,11 +15,38 @@ def index(request):
 			'username':user.username,
 			'firstname':user.first_name,
 			'lastname':user.last_name,
+			'event': None,
+			'event_list':Event.objects.all(),
+			'attendee_list':None,
 		}
 		print context
 		if request.method== 'POST' and 'logout' in request.POST:
 			logout(request)
 			return redirect('/')
+
+		if request.method == 'POST' and 'create_event' in request.POST:
+			event_title = request.POST['title']
+			event_text = request.POST['text']
+			event_creator = user.username
+			event = Event.objects.create(creator=event_creator, title=event_title, text=event_text)
+			event.save()
+			return redirect('/AttendanceLists/')
+
+		if request.method == 'GET' and request.GET.get('event_id'):
+			event_id = request.GET.get('event_id')
+			elist = Event.objects.filter(id = event_id)
+			if elist.exists():
+				print "-----------------------"
+				context['event'] = Event.objects.get(id=event_id)
+				context['attendee_list'] = Attendee.objects.filter(event = context['event'])
+				context['event_list'] = None
+		if request.method == 'POST' and 'add_attendee' in request.POST:
+			event_id = request.POST['event_id']
+			elist = Event.objects.filter(id = event_id)
+			if elist.exists():
+				attendee = Attendee.objects.create(name=request.POST['name'], event = Event.objects.get(id=event_id))
+				attendee.save()
+				return redirect('/AttendanceLists/?event_id=%s' % (event_id) ) 
 		return render(request, 'AttendanceLists/index.html', context)
 	else:
 		return redirect('/?redirected=True')
